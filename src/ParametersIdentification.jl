bb1f33553de1c2b0cb8e6e954f051825210c1a4f
ivborissov <iv.borissov@gmail.com>
2018-08-31 20:05:58 +0300

    d2d_ple fixed

--- src/ParametersIdentification.jl
+++ src/ParametersIdentification.jl
@@ -5,7 +5,8 @@ module ParametersIdentification
 # includes
 include("params_intervals.jl")
 include("params_plot.jl")
+include("params_d2d_ple.jl")
 # exports
-export params_intervals, params_plot
+export params_intervals, params_plot, params_intervals_d2d
 
 end
