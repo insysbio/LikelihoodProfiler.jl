fb939db9dece2c5dbc3838981305966301472c39
ivborissov <iv.borissov@gmail.com>
2018-09-14 11:36:31 +0300

    syntax changes in tests

--- test/simple_loss_func_test.jl
+++ test/simple_loss_func_test.jl
@@ -59,42 +59,42 @@ res_f_4p_3im = [params_intervals(
 # tests
 @testset "f_1p" begin
     @test all(@. isapprox(res_f_1p.intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_1p.ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
+    @test res_f_1p.ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
 end
 
 @testset "f_2p_1im" begin
     @test all(@. isapprox(res_f_2p_1im[1].intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_2p_1im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
-    @test all(res_f_2p_1im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
+    @test res_f_2p_1im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
+    @test res_f_2p_1im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
 end
 
 @testset "f_2p" begin
     @test all(@. isapprox(res_f_2p[1].intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_2p[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
+    @test res_f_2p[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
     @test all(@. isapprox(res_f_2p[2].intervals, [2.0, 6.0], atol=0.001))
-    @test all(res_f_2p[2].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
+    @test res_f_2p[2].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
 end
 
 @testset "f_3p_1im" begin
     @test all(@. isapprox(res_f_3p_1im[1].intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_3p_1im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
-    @test all(res_f_3p_1im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
-    @test all(res_f_3p_1im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
+    @test res_f_3p_1im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
+    @test res_f_3p_1im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
+    @test res_f_3p_1im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
 end
 
 @testset "f_4p_2im" begin
     @test all(@. isapprox(res_f_4p_2im[1].intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_4p_2im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
+    @test res_f_4p_2im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
     @test all(@. isapprox(res_f_4p_2im[2].intervals, [2.0, 6.0], atol=0.001))
-    @test all(res_f_4p_2im[2].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
-    @test all(res_f_4p_2im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
-    @test all(res_f_4p_2im[4].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
+    @test res_f_4p_2im[2].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
+    @test res_f_4p_2im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
+    @test res_f_4p_2im[4].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
 end
 
 @testset "f_4p_3im" begin
     @test all(@. isapprox(res_f_4p_3im[1].intervals, [1.0, 5.0], atol=0.001))
-    @test all(res_f_4p_3im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED])
-    @test all(res_f_4p_3im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
-    @test all(res_f_4p_3im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
-    @test all(res_f_4p_3im[4].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED])
+    @test res_f_4p_3im[1].ret_codes == [:FTOL_REACHED, :FTOL_REACHED]
+    @test res_f_4p_3im[2].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
+    @test res_f_4p_3im[3].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
+    @test res_f_4p_3im[4].ret_codes == [:BOUNDS_REACHED, :BOUNDS_REACHED]
 end
