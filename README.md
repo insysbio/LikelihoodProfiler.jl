767ece708422ea3bf5e76d2bea4d176e439a284f
ivborissov <iv.borissov@gmail.com>
2018-10-09 11:46:46 +0300

    travis docsdeploy added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LikelihoodProfiler
 
-[![Documentation](https://img.shields.io/badge/docs-stable-blue.svg)](https://insysbio.github.io/LikelihoodProfiler.jl)
+[![Documentation](https://img.shields.io/badge/docs-stable-blue.svg)](https://insysbio.github.io/LikelihoodProfiler.jl/stable)
 [![Travis](https://travis-ci.org/insysbio/LikelihoodProfiler.jl.svg?branch=master)](https://travis-ci.org/insysbio/LikelihoodProfiler.jl)
 [![AppVeyor](https://ci.appveyor.com/api/projects/status/8qdhx23slm9qc0m2?svg=true)](https://ci.appveyor.com/project/ivborissov/likelihoodprofiler-jl)
 
--- docs/make.jl
+++ docs/make.jl
@@ -3,19 +3,20 @@ using Documenter, LikelihoodProfiler
 
 makedocs(
     format = :html,
-    build   = "",
-    sitename = "LikelihoodProfiler",
+    #build   = "",
+    sitename = "LikelihoodProfiler.jl",
     pages = [
         "index.md",
         "basics.md"
     ],
-    repo = "https://github.com/insysbio/LikelihoodProfiler.jl/blob/master/docs/src/index.md"
+    #repo = "https://github.com/insysbio/LikelihoodProfiler.jl/blob/master/docs/src/index.md"
 )
 
 deploydocs(
-    repo   = "",
-    julia = "0.6",
-    osname = "windows",
+    repo   = "github.com/insysbio/LikelihoodProfiler.jl.git",
+    deps   = Deps.pip("mkdocs"),
+    julia = "0.7",
+    osname = "linux",
     target = "build",
     deps   = nothing,
     make = nothing
