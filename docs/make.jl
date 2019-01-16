a5efe9bf344094d8749412bb72eed96af1ce0489
ivborissov <iv.borissov@gmail.com>
2019-01-17 01:56:15 +0300

    mkdocs changed

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,13 @@ makedocs(
     # format = :html,
     # build   = "",
     sitename = "LikelihoodProfiler.jl",
+    #=
     pages = [
         "index.md",
         "methods.md",
         "api.md"
     ],
+    =#
     modules = [LikelihoodProfiler]
     #repo = "https://github.com/insysbio/LikelihoodProfiler.jl/blob/master/docs/src/index.md"
 )
