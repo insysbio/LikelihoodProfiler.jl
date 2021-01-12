aa823d8e89120d01d94cb95ad5f8bbe081009d0a
Ivan <iv.borissov@gmail.com>
2021-01-12 09:50:26 +0300

    auglag added

--- src/auglag.jl
+++ src/auglag.jl
@@ -1,109 +1,36 @@
-"""
-Implementation of an augmented Lagrangian method based on NLopt package.
-"""
+using NLopt, LinearAlgebra
 
-function auglag(
-    ::Val{NLopt},
-    theta_init::Vector{Float64}, # initial point of parameters
-    scan_func, # function returns scan value
-    loss_func; # function returns loss value
-    local_alg::LN_NELDERMEAD
-)
-    # optimizer
-
-    local_opt = Opt(local_alg, n_theta)
-    ftol_abs!(local_opt, scan_tol) #ftol_abs
-
-    # Constraints function: loss_val <= 0
-    out_of_bound = false
-    function constraints_func(x, g)
-        if length(g)>0
-            Calculus.finite_difference!(loss_func,x,g,:central)
-            #ForwardDiff.gradient!(g, loss_func, x)
-        end
-        try
-            scan_val = scan_func(x)
-            loss_val = loss_func(x)
-        catch e
-            msg = e.msg
-            @warn "Error when call loss_func($x) for loss_val. $msg"
-            throw(e)
-        end
-
-        # this part is necessary to understand the difference between
-        # "stop out of bounds" and "stop because of function call error"
-        if (loss_val < 0.) && (scan_val > scan_bound)
-            out_of_bound = true
-            throw(ForcedStop("Out of the scan bound but in ll constraint."))
-        #elseif isapprox(loss_val, 0., atol=loss_tol)
-            #@warn "loss_tol reached... but..."
-            #return loss_val
-        end
-
-        return loss_val
-    end
-
-    # condition for scan_val
-    opt = Opt(:LN_AUGLAG, n_theta)
-    ftol_abs!(opt, scan_tol)
-    max_objective!(
-        opt,
-        function(x, g)
-
-            if length(g)>0
-                Calculus.finite_difference!(scan_func,x,g,:central)
-                #ForwardDiff.gradient!(g,scan_func, x)
-            end
-            scan_func(x)
-        end
-        )
-    local_optimizer!(opt, local_opt)
-    maxeval!(opt, max_iter)
-
-    # inequality constraints
-    inequality_constraint!(
-        opt,
-        constraints_func,
-        loss_tol
-    )
-
-    function left_bound_func(x,grad,theta_bounds,i)
-        if length(grad)>0
-            #grad .= zeros(length(grad))
-            grad[i] = -1.0
-        end
-        theta_bounds[i][1] - x[i]
-    end
-
-    function right_bound_func(x,grad,theta_bounds,i)
-        if length(grad)>0
-            #grad .= zeros(length(grad))
-            grad[i] = 1.0
-        end
-        x[i] - theta_bounds[i][2]
-    end
-
-    [ inequality_constraint!(
-        opt,
-        (x, g) -> right_bound_func(x,g,theta_bounds,i),
-        0.
-    ) for i in 1:n_theta ]
-
-    [ inequality_constraint!(
-        opt,
-        (x, g) -> left_bound_func(x,g,theta_bounds,i),
-        0.
-    ) for i in 1:n_theta ]
-
-    # start optimization: (max scan_val, optimal params, code)
-
-    (optf, optx, ret) = optimize(opt, theta_init)
+struct PLModel{SF,LF,T}
+    scan_func::SF
+    loss_func::LF
+    x0::Vector{T}
+    lbounds::Vector{T}
+    ubounds::Vector{T}
+    scan_range::Tuple{T,T}
+    crit_level::T
+    #nvar::Int
 end
 
+mutable struct Auglag{F1,F2,T}
+    scan_func::F1
+    constr_func::F2
+    λ::T
+    μ::T
+    #η::T
+    #ω::T 
+    #η_final::T
+    #ω_final::T
+    direction::Int
+end
+
+update_direction!(auglag::Auglag, direction::Int) = auglag.direction = direction
+update_λ!(auglag::Auglag, λ) = auglag.λ = λ
+update_μ!(auglag::Auglag, μ) = auglag.μ = μ
+
+obj(auglag::Auglag) = (x,g)-> auglag.direction*auglag.scan_func(x) + auglag.λ*auglag.constr_func(x) + 0.5*auglag.μ*(auglag.constr_func(x))^2
+
 
 """
-    auglag(nlp)
-
 Implementation of an augmented Lagrangian method. The following keyword parameters can be passed:
 - μ: Starting value of the penalty parameter (default: 10.0)
 - atol: Absolute tolerance used in dual feasibility measure (default: 1e-8)
@@ -114,22 +41,176 @@ Implementation of an augmented Lagrangian method. The following keyword paramete
 - max_eval: Maximum number of objective function evaluations (default: 100000)
 - subsolver_logger: Logger passed to `tron` (default: NullLogger)
 """
-
-function auglag(
-    x0::Vector{T}, # initial point of parameters
-    scan_func, # function returns scan value
-    loss_func; # function returns loss value
-    subsolver::Symbol = :LN_NELDERMEAD,
-    μ::Real = eltype(x0)(10.0),
-    max_iter::Int = 1000,
+function identify_auglag(
+    model::PLModel,
+    subsolver::Symbol = :LN_NELDERMEAD;
+    μ::Real = 10.0, # penalty parameter
     max_time::Real = 30.0,
-    max_eval::Int=100000,
-    atol::Real = 1e-8,
-    rtol::Real = 1e-8,
-    ctol::Real = 1e-8,
-    stopval::Real = eltype(x0)(1e9)
-) where T
-    T = eltype(x0)
+    max_eval::Int = 10000,
+    #atol::Real = 1e-8, 
+    #rtol::Real = 1e-8, 
+    converge_tol::Real = 1e-3,
+    constr_tol::Real = 1e-3
+)   
+    T = eltype(model.x0)
+    lb = model.lbounds
+    ub = model.ubounds
 
+    x0 = model.x0
+    x0 .= max.(lb, min.(x0, ub))
+    nvar = length(x0)
 
+    scan_func = model.scan_func
+    loss_func = model.loss_func
+    constr_func(x) = loss_func(x) - model.crit_level
+
+    # Final tolerances
+    η_final = constr_tol
+    ω_final = converge_tol
+
+    # Starting values as in Nocedal
+    μ₀ = 10.
+    ω₀ = ω_final #1/μ₀ # not implemented
+    η₀ = 1/μ₀^0.1
+
+    # Init lagrange multiplier
+    λ₀ = 1.0
+
+    # Init Augmented Lagrangian
+    auglag = Auglag(scan_func, constr_func, λ₀, μ₀, 1)
+
+    results = Vector{Any}(undef, 2)
+    for (i, endpoint) in enumerate((:lower, :upper))
+        
+        x = copy(x0)
+        μ = μ₀; ω = ω₀; η = η₀
+
+        # Lagrange setup
+        direction = sign_of_direction(Val{endpoint})
+        λ = λ₀
+        update_direction!(auglag, direction)
+        update_λ!(auglag, λ)
+        update_μ!(auglag, μ)
+
+        iter = 0
+        evals_tot = 0
+        start_time = time()
+        el_time = 0.0
+
+        ep = scan_func(x)
+        loss = loss_func(x)
+        status = :none
+
+        while status ∉ (:identifiable,  :unidentifiable,  :failure,  :tired)
+            
+            # solve subproblem
+            (optf, optx, ret_code, evals) = subproblem_opt(obj(auglag), x; lb=lb, ub=ub, alg=subsolver, tol=ω, max_eval=max_eval)
+            
+            if ret_code in (:FTOL_REACHED, :XTOL_REACHED, :SUCCESS)
+                opt_status = :success
+            elseif ret_code in (:MAXEVAL_REACHED, :MAXTIME_REACHED)
+                opt_status = :tired
+            else
+                opt_status = :failure
+            end
+
+            # evals and time measure
+            iter += 1
+            evals_tot += evals
+            el_time = time() - start_time
+            opt_status = evals_tot > max_eval || el_time > max_time ? :tired : opt_status
+
+            if opt_status == :success
+                # new optimal x
+                x .= optx
+
+                # endpoint estimation
+                scan_val = scan_func(x)
+
+                # Norm of constr function
+                cf = constr_func(x)
+                normcf = norm(cf)
+               # @show x
+               # @show cf
+               # @show scan_val
+               # @show cf ≤ 0.0
+               # @show !(model.scan_range[1] ≤ scan_val ≤ model.scan_range[2])
+                # Test unidentifiability
+                if cf ≤ 0.0 && !(model.scan_range[1] ≤ scan_val ≤ model.scan_range[2])
+                    
+                    ep = nothing
+                    loss = loss_func(x)
+                    status = :unidentifiable
+                    break
+                end
+
+                # Test identifiability
+                if normcf ≤ η
+                    # test for convergence
+                    if normcf ≤ η_final  # && normgp ≤ ϵd gradient projection method not implemented
+                        ep = scan_val
+                        loss = loss_func(x)
+                        status = :identifiable
+                        break
+                    end
+                    # update multipliers, tighten tolerances
+                    update_λ!(auglag, auglag.λ-auglag.μ*constr_func(x))
+                    η = max(η/auglag.μ^0.9, η_final)
+                    # ω = ω/auglag.μ # not implemented
+                else
+                    # increase penalty parameter, tighten tolerances
+                    update_μ!(auglag,100*auglag.μ)
+                    η = max(η/auglag.μ^0.1, η_final)
+                    # ω = 1/auglag.μ # not implemented
+                end
+            else
+                ep = nothing
+                loss = nothing
+                status = opt_status
+            end
+        end
+        
+        results[i] = (ep,loss,status,evals_tot,el_time)
+    end
+
+    return results
 end
+
+
+function identify_endpoint(auglag::Auglag, x0, )
+    
+end
+
+
+function subproblem_opt(lagrange_obj, x; lb=fill(1e-9,length(x)), ub=fill(1e9,length(x)), alg=:LN_NELDERMEAD, tol=1e-4, max_eval=50000)
+    
+    # NLopt optimization problem 
+    nparams = length(x)
+    opt = Opt(alg, nparams)
+    
+    # Box constraints
+    opt.lower_bounds = lb
+    opt.upper_bounds = ub
+
+    # Tolerance setup - ??
+    ftol_abs!(opt, tol)
+
+    maxeval!(opt, max_eval)
+
+    min_objective!(opt, lagrange_obj)
+
+    (optf, optx, ret) = optimize(opt, x)
+
+    return (optf, optx, ret, opt.numevals)
+end
+
+sign_of_direction(::Type{Val{:lower}}) = 1
+sign_of_direction(::Type{Val{:upper}}) = -1
+
+
+# test
+model_1p = PLModel((x)->x[1], (x)->5.0 + (x[1]-3.0)^2, [3.],[-1e-9],[1e9], (-1e-11,100.), 9.0)
+model_2p = PLModel((x)->x[2], (x)->5.0 + (x[1]-3.0)^2 + (x[2]-4.0)^2, [3.,4.],[-1e-9,-1e-9],[1e9,1e9], (-1e-11,100.), 9.0)
+model_2p = PLModel((x)->x[2], (x)->5.0 + (x[1]-3.0)^2 + (x[2]-4.0)^2, [3.,4.],[-1e-9,-1e-9],[1e9,1e9], (-1e-11,100.), 9.0)
+model_2p_1im = PLModel((x)->x[2], (x)->5.0 + (x[1]-3.0)^2 + 0*x[2], [3.,4.],[-1e-9,-1e-9],[1e9,1e9], (-1e-11,100.), 9.0)
+model_3p_1im = PLModel((x)->x[3], (x)->5.0 + (x[1]-3.0)^2 + (x[2]/x[3]-4.0)^2, [3.,4.,3.0],[-1e-9,-1e-9,1e-9],[1e9,1e9,1e9], (-1e-11,100.), 9.0)
---
+++ src/auglag2.jl
@@ -0,0 +1,193 @@
+using NLopt, LinearAlgebra
+
+# structure representing NLModel
+struct PLModel{SF,LF,T}
+    scan_func::SF
+    loss_func::LF
+    x0::Vector{T}
+    lbounds::Vector{T}
+    ubounds::Vector{T}
+    scan_range::Tuple{T,T}
+    crit_level::T
+    #nvar::Int
+end
+
+# structure holding Lagrangian related data
+mutable struct Auglag{F1,F2,T}
+    scan_func::F1
+    constr_func::F2
+    #loss_func::F2
+    #crit_level::T
+    λ::T
+    μ::T
+    #direction::Int
+end
+
+obj(auglag::Auglag, direction) = (x,g) -> direction*auglag.scan_func(x) + 
+  auglag.λ*auglag.constr_func(x) + 
+    0.5*auglag.μ*(auglag.constr_func(x))^2
+
+init_λ!(auglag::Auglag, λ) = auglag.λ = λ
+init_μ!(auglag::Auglag, μ) = auglag.μ = μ
+update_λ!(auglag::Auglag, x) = auglag.λ = auglag.λ-auglag.μ*auglag.constr_func(x)
+update_μ!(auglag::Auglag) = auglag.μ = 100*auglag.μ
+
+function identify_auglag(
+  model::PLModel,
+  subsolver::Symbol = :LN_NELDERMEAD;
+  max_time::Real = 30.0,
+  max_eval::Int = 10000,
+  #atol::Real = 1e-8, 
+  #rtol::Real = 1e-8, 
+  converge_tol::Real = 1e-3,
+  constr_tol::Real = 1e-3
+)   
+
+  lbounds = model.lbounds
+  ubounds = model.ubounds
+
+  x0 = model.x0
+  x0 .= max.(lbounds, min.(x0, ubounds))
+
+  # Final tolerances
+  η_final = constr_tol
+  ω_final = converge_tol
+
+  # Starting values as in Nocedal
+  μ₀ = 10.
+  ω₀ = ω_final # 1/μ₀ # not implemented
+  η₀ = 1/μ₀^0.1
+
+  # Init lagrange multiplier
+  λ₀ = 1.0
+
+  # Init Augmented Lagrangian
+  constr_func(x) = model.loss_func(x) - model.crit_level
+  auglag = Auglag(model.scan_func, constr_func, λ₀, μ₀)
+
+  ep_lower = identify_endpoint(auglag, x0, :lower, η₀, ω₀, η_final, ω_final, model.scan_range;
+    alg=subsolver, lb=lbounds, ub=ubounds, max_time=max_time, max_eval=max_eval) # can be changed in Julia 1.5.3
+
+  init_λ!(auglag, λ₀)
+  init_μ!(auglag, μ₀)
+
+  ep_upper = identify_endpoint(auglag, x0, :upper, η₀, ω₀, η_final, ω_final, model.scan_range;
+    alg=subsolver, lb=lbounds, ub=ubounds, max_time=max_time, max_eval=max_eval) # can be changed in Julia 1.5.3
+
+  return (ep_lower, ep_upper)
+end
+
+function identify_endpoint(auglag, x, ep_type, η, ω, η_final, ω_final, scan_range; 
+  max_time=30.0, max_eval=10000, kwargs...)
+  
+  # auglag functions
+  scan_func = auglag.scan_func
+  constr_func = auglag.constr_func
+
+  # search direction
+  direction_sign = ep_type == :lower ? 1 : -1
+
+  iter = 0
+  evals_tot = 0
+  start_time = time()
+  el_time = 0.0
+
+  ep = scan_func(x)
+  constr_viol = constr_func(x)
+  status = :none
+
+  while status ∉ (:identifiable,  :unidentifiable,  :failure,  :tired)
+            
+    # solve subproblem
+    (optf, optx, ret_code, evals) = subproblem_opt(obj(auglag,direction_sign), x; 
+      tol=ω, kwargs...)
+
+    if ret_code in (:FTOL_REACHED, :XTOL_REACHED, :SUCCESS)
+      opt_status = :success
+    elseif ret_code in (:MAXEVAL_REACHED, :MAXTIME_REACHED)
+      opt_status = :tired
+    else
+      opt_status = :failure
+    end
+
+    # evals and time measure
+    iter += 1
+    evals_tot += evals
+    el_time = time() - start_time
+    opt_status = evals_tot > max_eval || el_time > max_time ? :tired : opt_status
+
+    if opt_status == :success
+      # new optimal x
+      x .= optx
+
+      # endpoint estimation
+      scan_val = scan_func(x)
+
+      # Norm of constr function
+      cf = constr_func(x)
+      normcf = norm(cf)
+
+      # Test unidentifiability
+      if cf ≤ 0.0 && !(scan_range[1] ≤ scan_val ≤ scan_range[2])
+                    
+        ep = nothing
+        constr_viol = constr_func(x)
+        status = :unidentifiable
+        break
+      end
+
+      # Test identifiability
+      if normcf ≤ η
+        # test for convergence
+        if normcf ≤ η_final  # && normgp ≤ ϵd gradient projection method not implemented
+          ep = scan_val
+          constr_viol = constr_func(x)
+          status = :identifiable
+          break
+        end
+        # update multipliers, tighten tolerances
+        update_λ!(auglag, x)
+        η = max(η/auglag.μ^0.9, η_final)
+        # ω = ω/auglag.μ # not implemented
+      else
+        # increase penalty parameter, tighten tolerances
+        update_μ!(auglag)
+        η = max(η/auglag.μ^0.1, η_final)
+        # ω = 1/auglag.μ # not implemented
+      end
+
+    else
+      ep = nothing
+      constr_viol = nothing
+      status = opt_status
+    end
+  end
+
+  return (ep,constr_viol,status,evals_tot,el_time)
+end
+
+function subproblem_opt(lagrange_obj, x; 
+  lb=fill(1e-9,length(x)), ub=fill(1e9,length(x)), alg=:LN_NELDERMEAD, tol=1e-4, max_eval=50000, max_time=30.0)
+    
+  # NLopt optimization problem 
+  nparams = length(x)
+  opt = Opt(alg, nparams)
+  
+  # Box constraints
+  opt.lower_bounds = lb
+  opt.upper_bounds = ub
+
+  # Tolerance setup - ??
+  ftol_abs!(opt, tol)
+
+  maxeval!(opt, max_eval)
+
+  min_objective!(opt, lagrange_obj)
+
+  (optf, optx, ret) = optimize(opt, x)
+
+  return (optf, optx, ret, opt.numevals)
+end
+
+# tests
+model_1p = PLModel((x)->x[1], (x)->5.0 + (x[1]-3.0)^2, [3.],[-1e-9],[1e9], (-100.,100.), 9.0)
