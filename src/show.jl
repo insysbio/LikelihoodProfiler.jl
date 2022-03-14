4f6838dbfc6eb09cf8d8061e6217c31f95a4fcc8
Evgeny Metelkin <metelkin@insysbio.com>
2022-03-14 22:30:48 +0300

    sigdigits for round

--- src/show.jl
+++ src/show.jl
@@ -12,18 +12,18 @@ end
 
 function Base.show(io::IO, mime::MIME"text/plain", pe::ParamInterval)
   left_interval_point = if pe.result[1].status === :BORDER_FOUND_BY_SCAN_TOL || pe.result[1].status === :BORDER_FOUND_BY_LOSS_TOL
-    round(pe.result[1].value, digits=3)
+    round(pe.result[1].value, sigdigits=4)
   elseif pe.result[1].status === :SCAN_BOUND_REACHED
     bound_1 = pe.input.scan_bounds[1]
-    "<$(round(bound_1, digits=3))"
+    "<$(round(bound_1, sigdigits=4))"
   else
     pe.result[1].value
   end
   right_interval_point = if pe.result[2].status === :BORDER_FOUND_BY_SCAN_TOL || pe.result[2].status === :BORDER_FOUND_BY_LOSS_TOL
-    round(pe.result[2].value, digits=3)
+    round(pe.result[2].value, sigdigits=4)
   elseif pe.result[2].status === :SCAN_BOUND_REACHED
     bound_2 = pe.input.scan_bounds[2]
-    ">$(round(bound_2, digits=3))"
+    ">$(round(bound_2, sigdigits=4))"
   else
     pe.result[2].value
   end
