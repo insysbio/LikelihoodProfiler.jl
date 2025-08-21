9f92001088ab812360fc41f5534d0cf589e51320
Ivan Borisov <iv.borissov@gmail.com>
2025-08-21 10:54:54 +0300

    Update deprecated.jl

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,13 +1,17 @@
-profile(plprob::ProfileLikelihoodProblem{ParameterProfile}, method::AbstractProfilerMethod; 
+function profile(plprob::ProfileLikelihoodProblem{ParameterProfile}, method::AbstractProfilerMethod; 
   idxs::AbstractVector{<:Int} = eachindex(get_optpars(plprob)),
-  parallel_type::Symbol=:none, kwargs...) = solve(plprob, method; idxs=idxs, parallel_type=parallel_type, kwargs...)
+  parallel_type::Symbol=:none, kwargs...) 
+  Base.depwarn("`profile` is deprecated. Please use `solve` instead.", :profile, force = true)
 
-Base.depwarn("`profile` is deprecated. Please use `solve` instead.", :profile)
+  return solve(plprob, method; idxs=idxs, parallel_type=parallel_type, kwargs...)
+end
 
-PLProblem(optprob::OptimizationProblem, optpars::AbstractVector{<:Real},
+
+function PLProblem(optprob::OptimizationProblem, optpars::AbstractVector{<:Real},
   profile_range::Union{AbstractVector, Tuple} = tuple.(optprob.lb, optprob.ub); 
-  conf_level::Float64 = 0.95, df::Int = 1, threshold::Real = chi2_quantile(conf_level, df)) = 
-    ProfileLikelihoodProblem(optprob, optpars, profile_range=profile_range; 
-      conf_level=conf_level, df=df, threshold=threshold)
+  conf_level::Float64 = 0.95, df::Int = 1, threshold::Real = chi2_quantile(conf_level, df))
+  Base.depwarn("`PLProblem` is deprecated. Please use `ProfileLikelihoodProblem` instead.", :PLProblem, force = true)
 
-Base.depwarn("`PLProblem` is deprecated. Please use `ProfileLikelihoodProblem` instead.", :PLProblem)
+  return ProfileLikelihoodProblem(optprob, optpars, profile_range; 
+    conf_level=conf_level, df=df, threshold=threshold)
+end
